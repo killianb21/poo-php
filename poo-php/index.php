051b301de0c37ea5376b9a4bc18d01bf0d522abe
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-11-15 10:31:26 +0100

    Créez vos propres classes

--- poo-php/index.php
+++ poo-php/index.php
@@ -38,3 +38,23 @@ echo sprintf(
 );
 
 exit(0);
+
+class Player{
+    public int $level;
+}
+
+class Encounter{
+    function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+{
+    return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
+}
+
+function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+{
+    if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
+        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+    }
+
+    $levelPlayerOne += (int) (32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+}
+}
