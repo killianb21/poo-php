a1f0d6a6607d02196ce5b4a52d29292bda8fd33b
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-11-15 11:22:01 +0100

    Utilisez les propriétés et méthodes statiques

--- poo-php/index.php
+++ poo-php/index.php
@@ -1,5 +1,8 @@
 <?php
 
+/*Greg à 9.09% chance de gagner face a Jade les niveaux des joueurs ont évolués vers 429 pour Greg et 740 pour Jade*/
+
+
 const RESULT_WINNER = 1;
 const RESULT_LOSER = -1;
 const RESULT_DRAW = 0;
@@ -24,12 +27,12 @@ $jade = 800;
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
-    probabilityAgainst($greg, $jade)*100
+    Encounter::probabilityAgainst($greg, $jade)*100
 ).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-setNewLevel($greg, $jade, RESULT_WINNER);
-setNewLevel($jade, $greg, RESULT_LOSER);
+Encounter::setNewLevel($greg, $jade, RESULT_WINNER);
+Encounter::setNewLevel($jade, $greg, RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
@@ -40,21 +43,27 @@ echo sprintf(
 exit(0);
 
 class Player{
-    public int $level;
+    public $level;
 }
 
 class Encounter{
-    function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    const RESULT_WINNER = 1;
+    const RESULT_LOSER = -1;
+    const RESULT_DRAW = 0;
+    const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
+
+
+public static function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
 {
     return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
 }
 
-function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+public static function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
 {
-    if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
-        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+    if (!in_array($playerOneResult, self::RESULT_POSSIBILITIES)) {
+        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
     }
 
-    $levelPlayerOne += (int) (32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+    $levelPlayerOne += (int) (32 * ($playerOneResult - self::probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
 }
 }
